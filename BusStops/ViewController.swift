48b10abae9744a2aceb1bc0a47c91c5be87cef02
Matt D. Bearson <matt.d.bearson@accenture.com>
2016-12-10 19:06:10 -0800

    triv

--- BusStops/ViewController.swift
+++ BusStops/ViewController.swift
@@ -72,7 +72,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         array.appendSection(sectionTwo)
         
